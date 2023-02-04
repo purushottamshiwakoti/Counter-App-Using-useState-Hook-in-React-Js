780b0f8be68556aaf0f4931241ac68ad7dcc2d71
Purushottam <puru.shiw2@gmail.com>
2023-02-04 13:08:07 +0545

    Finalize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(10);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="center">
+     <h1>Counter App</h1>
+     <div className="middle">
+     <button onClick={()=>(count===0)?setCount(0):setCount(count-1)}>-</button>
+     <h2>{count}</h2>
+     <button onClick={()=>setCount(count+1)}>+</button>
+     </div>
+     </div>
+      
+
     </div>
   );
 }
